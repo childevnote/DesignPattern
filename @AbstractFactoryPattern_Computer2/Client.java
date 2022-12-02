ab5cec42e3630731aa619c8a98e00e5f3992261e
daniel11251 <daniel011125@gmail.com>
2022-12-02 19:50:59 +0900

    추상팩토리패턴 녹강

--- @AbstractFactoryPattern_Computer2/Client.java
+++ @AbstractFactoryPattern_Computer2/Client.java
@@ -3,9 +3,14 @@ public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ComputerFactory computer = new ComputerFactory();
+		/*ComputerFactory computer = new ComputerFactory();
+		computer.createComputer("LG");
+		computer.createComputer("Samsung");*/
+		
+		FactoryofComputerFactory computer = new FactoryofComputerFactory();
 		computer.createComputer("LG");
 		computer.createComputer("Samsung");
+		
 		
 	}
 
